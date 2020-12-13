de1de23c613e36169da2fb599da3f882109b8d5a
Leonid Medovyy <leonid@logik.works>
2020-12-12 18:00:01 -0700

    setup microdata

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,17 @@
+# frozen_string_literal: true
+
 class PagesController < ApplicationController
+  require 'rich_text_renderer'
+  before_action :create_renderer
+
   def index
     @page_title = 'Publish Pro | Making internet a better place'
     @page_description = ''
+  end
+
+  private
+
+  def create_renderer
+    @renderer = RichTextRenderer::Renderer.new
   end
 end
